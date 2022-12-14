2b021174652d88b39c6c9daf6092ea49430d4e79
Riku0413 <riku.k.18.soccer@gmail.com>
2022-12-14 20:51:52 +0900

    made mylife4.c and gol4.h for the assignment 4.

---
+++ lifegame/soft2-lec01-mac-m1/gol4.h
@@ -0,0 +1,363 @@
+// 課題3のためのファイル
+
+void init_cells_4(const int height, const int width, int cell[height][width], FILE* fp) {
+
+  // ランダムな初期化
+  if (fp == NULL) {
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        double x = (double)rand() / RAND_MAX;
+          if (x > 0.9) {
+            if (i < 20) {
+              cell[i][j] = 1;
+            } else {
+              cell[i][j] = 10;
+            }
+          }
+      }
+    }
+  }
+  // ファイルを指定して初期化
+  else {
+    int x, y;
+    char s[3000];
+    fscanf(fp, "%[^\n]", s); // 最初の\nまでをスキャン
+
+    // #Life 1.06 の場合
+    if (s[1] == 'L' && s[2] == 'i') { // 十分条件
+
+      while (fscanf(fp, "%d %d", &x, &y) != EOF) {
+        if (y < 20) {
+          cell[y][x] = 1; // 上半分は 赤 勢力
+        } else {
+          cell[y][x] = 10; // 下半分は 青 勢力
+        }
+      }
+    }
+    
+    // RLE の場合
+    else {
+      // 説明文が終わるまでスキャン
+      if (s[0] != 'x') {
+        while(1) {
+          fscanf(fp, "%[^\n]", s);
+          if (s[0] == 'x') {
+            break;
+          }
+        }
+      }
+
+      // 本文の読み込み
+
+      // 一旦全部読み込み
+      fscanf(fp, "%[^!]", s);
+
+      char data[100];
+      int p_s = 0;
+      int p_data = 0;
+      int line = 0; // 何行目のセルかを指定
+      int raw = 0; // 何列目のセルかを指定
+
+      while (1) {
+        if (s[p_s] == '\n' | s[p_s] == ' ') {
+          p_s++;
+          continue;
+        }
+
+        data[p_data] = s[p_s];
+
+        if (s[p_s] == '$' | s[p_s] == '!') {
+          // ここで1行のセル更新
+          int i = 0;
+          char buff[3]; // \0の枠も確保!
+          buff[0] = '0'; buff[1] = '0'; buff[2] = '\0';// デフォルトでは文字の0 -> bobo みたいな符号にも対応！！
+          int rank = 0;
+          while (data[i] != '$' && data[i] != '!') { // 1行が終わるまでのループ
+            if (data[i] >= '0' && data[i] <= '9') {
+              buff[rank] = data[i];
+              rank++;
+            }
+            else { // b か o のとき
+              buff[rank] = '\0';
+              int num = atoi(buff);
+              if (num == 0) {
+                num = 1;
+              }
+              for (int k = 0; k < num; k++) {
+                // if (data[i] == 'b') {
+                //   cell[line][raw] = 0;
+                // }
+                if (data[i] == 'o') {
+                  if (line < 20) {
+                    cell[line][raw] = 1; // 赤
+                  } else {
+                    cell[line][raw] = 10; // 青
+                  }
+                }
+                raw++;
+              }
+              rank = 0;
+              buff[0] = '0'; buff[1] = '0';
+            }
+            i++;
+          }
+          raw = 0;
+          line++;
+          p_data = 0; // リセット
+          if (s[p_s] == '$') {
+            p_s++;
+            continue; // 次の行へ
+          } else { // '!'のとき
+            break;
+          }
+        }
+        p_data++;
+        p_s++;
+      }
+      p_s = 0;
+    }
+  }
+  fclose(fp);
+}
+
+// グリッドの描画: 世代情報とグリッドの配列等を受け取り、ファイルポインタに該当する出力にグリッドを描画する
+void print_cells_4(FILE *fp, int gen, const int height, const int width, int cell[height][width]) {
+  // 比率の調査
+  // double ratio = 0;
+  // for (int i = 0; i < height; i++) {
+  //     for (int j = 0; j < width; j++) {
+  //       if (cell[i][j] == 1) {
+  //         ratio += 1;
+  //       }
+  //     }
+  // }
+  // ratio /= (height * width);
+  //
+
+  // 残兵数の調査
+  int red = 0;
+  int blue = 0;
+  for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (cell[i][j] == 1) {
+          red += 1;
+        } else if (cell[i][j] == 10) {
+          blue += 1;
+        }
+      }
+  }
+  //
+
+  system("clear"); // コンソールのクリア
+  printf("generation = %d, red = %d, blue = %d\r\n", gen, red, blue);
+  printf("+");
+  for (int i = 0; i < 70; i++) {
+    printf("-");
+  }
+  printf("+\r\n");
+
+  for (int i = 0; i < 40; i++) {
+    printf("|");
+    for (int j = 0; j < 70; j++) {
+      if (cell[i][j] == 0) {
+        printf(" ");
+      } else if (cell[i][j] == 1) {
+        printf("\x1b[31m#\x1b[0m"); // 赤 のプリント
+      } else { // 10 のとき
+        printf("\x1b[34m#\x1b[0m"); // 青 のプリント
+      }
+    }
+    printf("|\r\n");
+  }
+
+  printf("+");
+  for (int i = 0; i < 70; i++) {
+    printf("-");
+  }
+  printf("+\r\n");
+}
+
+void update_cells_4(const int height, const int width, int cell[height][width]) {
+  int cell_expanded[height + 2][width + 2];
+  // 全てのセルを対等に扱うために盤面をexpand
+  for (int i = 0; i < height + 2; i++) {
+      for (int j = 0; j < width + 2; j++) {
+        cell_expanded[i][j] = 0;
+      }
+  }
+  // 現在の盤面をコピー
+  for (int i = 1; i < height + 1; i++) {
+      for (int j = 1; j < width + 1; j++) {
+        cell_expanded[i][j] = cell[i - 1][j - 1];
+      }
+  }
+
+  // 世代交代
+  // まずは射程範囲に敵がいるかどうか
+  // いたらその数的比率によって生死が分かれる
+  // 倒す確率ではなく死ぬ確率！
+  // いない場合は探索して移動
+
+  int flag = 0;
+  int count_red = 0;
+  int count_blue = 0;
+  for (int i = 1; i < height + 1; i++) {
+    for (int j = 1; j < width + 1; j++) {
+      if (cell_expanded[i][j] == 1) {
+        count_red++;
+      } else if (cell_expanded[i][j] == 10) {
+        count_blue++;
+      }
+    }
+  }
+  if (count_red > 0 && count_blue > 0) {
+    flag = 100; // 戦争フラグ
+  } else if (count_red > 0) {
+    flag = 1;
+  } else {
+    flag = 10;
+  }
+
+  // 戦争時のムーブ
+  if (flag == 100) {
+
+    // 乱数の準備
+    srand((unsigned)time(NULL));
+
+    for (int i = 1; i < height + 1; i++) {
+      for (int j = 1; j < width + 1; j++) {
+
+        if (cell_expanded[i][j] == 0) {
+          continue; // 空き地なら次のマスへ
+        }
+
+        // 人が存在するとき
+        int count = 0; // 敵の数を数える
+        count += cell_expanded[i - 1][j - 1];
+        count += cell_expanded[i - 1][j];
+        count += cell_expanded[i - 1][j + 1];
+        count += cell_expanded[i][j - 1];
+        count += cell_expanded[i][j + 1];
+        count += cell_expanded[i + 1][j - 1];
+        count += cell_expanded[i + 1][j];
+        count += cell_expanded[i + 1][j + 1];
+
+        if (cell_expanded[i][j] == 1) { // 赤のとき
+          count /= 10; // 敵（青）の数
+        }
+        else { // 青のとき
+          count %= 10; // 敵（赤）の数
+        }
+
+        if (count > 0) { // 周囲に一人でも敵がいるとき
+          double p = count / 8.0; // 殺される確率    // 型変換？？？？？？？
+          double x = (double)rand() / RAND_MAX; // 0 ~ 1
+          if (x < p) {
+            cell[i - 1][j - 1] = 0; // 戦死 // 更新するのはcell！！！！！！！！！
+          }
+        }
+        else { // 周囲に敵がいないとき
+
+          // 最寄りの敵を探す
+          int i_enemy = 200;
+          int j_enemy = 200;
+          for (int i2 = 1; i2 < height + 1; i2++) {
+            for (int j2 = 1; j2 < width + 1; j2++) {
+              if (i2 == i && j2 == j) { // 自分自身のマスは飛ばす
+                continue;
+              }
+              else if (cell_expanded[i2][j2] != 0 && cell_expanded[i][j] != cell_expanded[i2][j2]) {// 注目地点に"敵"がいるとき
+                if ((i - i2)*(i - i2) + (j - j2)*(j - j2) < (i - i_enemy)*(i - i_enemy) + (j - j_enemy)*(j - j_enemy)) {
+                  i_enemy = i2;
+                  j_enemy = j2;
+                } // 少なくとも一人は敵がいるはず！
+              }
+            }
+          }
+
+          // 敵に最も近づける位置をサーチ
+          int coor[8][2] = {{i - 1, j - 1}, {i - 1, j}, {i - 1, j + 1}, {i, j - 1}, {i, j + 1}, {i + 1, j - 1}, {i + 1, j}, {i + 1, j + 1}};
+          int i_next = i; // とりあえずstayを仮定
+          int j_next = j; // 同上
+          for (int a = 0; a < 8; a++) {
+            if ((coor[a][0] - i_enemy)*(coor[a][0] - i_enemy) + (coor[a][1] - j_enemy)*(coor[a][1] - j_enemy) < (i_next - i_enemy)*(i_next - i_enemy) + (j_next - j_enemy)*(j_next - j_enemy)) {
+              // 下の条件を満たせばそこに動ける！
+              if (cell[coor[a][0] - 1][coor[a][1] - 1] == 0 && (coor[a][0] > 0 && coor[a][0] < height + 1 && coor[a][1] > 0 && coor[a][1] < width + 1)) {
+                i_next = coor[a][0];
+                j_next = coor[a][1];
+              }
+            }
+          }
+          cell[i - 1][j - 1] = 0;
+          cell[i_next - 1][j_next - 1] = cell_expanded[i][j];
+        }
+
+      }
+    }
+  }
+
+  // 平穏時代のムーブ
+  // if (flag != 100) {
+  //   // とりあえず移動
+  //   // 移動するつもりだったところに誰かいたらstay
+  //   // 移動が終わった後のループで空のマスに誕生
+  //   // ratio が 0.3 を超えたら 上下片方の色を転換する！！
+
+  //   // 乱数の準備
+  //   srand((unsigned)time(NULL));
+
+  //   for (int i = 1; i < height + 1; i++) {
+  //     for (int j = 1; j < width + 1; j++) {
+
+  //       if (cell_expanded[i][j] == 0) {
+  //         continue; // 空き地なら次のマスへ
+  //       }
+
+  //       int coor[9][2] = {{i - 1, j - 1}, {i - 1, j}, {i - 1, j + 1}, {i, j - 1}, {i, j}, {i, j + 1}, {i + 1, j - 1}, {i + 1, j}, {i + 1, j + 1}};
+  //       int i_next = i;
+  //       int j_next = j;
+  //       double x = (double)rand() / RAND_MAX; // 0 ~ 1
+  //       x *= 9;
+  //       int y = x; // 0 ~ 8 の整数
+  //       if (cell[coor[y][0] - 1][coor[y][1] - 1] == 0 && (coor[y][0] > 0 && coor[y][0] < height + 1 && coor[y][1] > 0 && coor[y][1] < width + 1)) {
+  //         i_next = coor[y][0];
+  //         j_next = coor[y][1];
+  //       }
+  //       // 移動
+  //       cell[i - 1][j - 1] = 0;
+  //       cell[i_next - 1][j_next - 1] = cell_expanded[i][j];
+
+  //     }
+  //   }
+
+  //   // 誕生
+  //   for (int i = 1; i < height + 1; i++) {
+  //     for (int j = 1; j < width + 1; j++) {
+  //       if (cell[i - 1][j - 1] != 0) {
+  //         continue;
+  //       }
+  //       // 空のセルについて
+  //       int coor[8][2] = {{i - 1, j - 1}, {i - 1, j}, {i - 1, j + 1}, {i, j - 1}, {i, j + 1}, {i + 1, j - 1}, {i + 1, j}, {i + 1, j + 1}};
+  //       int parent = 0;
+  //       for (int a = 0; a < 8; a++) {
+  //         if (cell_expanded[coor[a][0]][coor[a][1]] != 0) { // 移動前の状態で、親が近くにいるか
+  //           parent++;
+  //         }
+  //       }
+
+  //       if (parent >= 2) {
+  //         double z = (double)rand() / RAND_MAX; // 0 ~ 1
+  //         if (z > 0.5) {
+  //           cell[i - 1][j - 1] = flag;
+  //         }
+  //       }
+
+  //     }
+  //   }
+
+  //   // ここで総数をカウントして、1割？を超えていたらセルの上半分を赤1、下半分を青10に！
+
+  // }
+
+}
---
+++ lifegame/soft2-lec01-mac-m1/mylife4.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // sleep()関数を使う
+#include <time.h>
+#include "gol4.h"
+
+int main(int argc, char **argv) {
+    FILE *fp = stdout;
+    const int height = 40;
+    const int width = 70;
+
+    int cell[height][width];
+    for(int y = 0 ; y < height ; y++){
+        for(int x = 0 ; x < width ; x++){
+            cell[y][x] = 0;
+        }
+    }
+
+    /* ファイルを引数にとるか、ない場合はデフォルトの初期値を使う */
+    if ( argc > 2 ) {
+        fprintf(stderr, "usage: %s [filename for init]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // ここの中を,lifとrleで分岐させる
+    else if (argc == 2) {
+        FILE *lgfile;
+        if ( (lgfile = fopen(argv[1],"r")) != NULL ) {
+            init_cells_4(height, width, cell, lgfile);
+        }
+        else {
+            fprintf(stderr,"cannot open file %s\n",argv[1]);
+            return EXIT_FAILURE;
+        }
+    }
+    //
+
+    else{
+        init_cells_4(height, width, cell, NULL);
+        // srand((unsigned)time(NULL));
+        // for (int i = 0; i < height; i++) {
+        //     for (int j = 0; j < width; j++) {
+        //           double x = (double)rand() / RAND_MAX;
+        //           if (x > 0.9) {
+        //             cell[i][j] = 1;
+        //           } else {
+        //             cell[i][j] = 0;
+        //           }
+        //     }
+        // }
+    }
+    
+    print_cells_4(fp, 0, height, width, cell); // 表示する
+    sleep(1); // 1秒休止
+    fprintf(fp,"\e[%dA",height+3);//height+3 の分、カーソルを上に戻す(壁2、表示部1)
+    
+    /* 世代を進める*/
+    for (int gen = 1 ;; gen++) {
+        update_cells_4(height, width, cell); // セルを更新
+        print_cells_4(fp, gen, height, width, cell);  // 表示する
+        sleep(1); //1秒休止する
+        fprintf(fp,"\e[%dA",height+3);//height+3 の分、カーソルを上に戻す(壁2、表示部1)
+
+        if (gen % 100 == 0 && gen < 1000) {
+            // ファイル名の調整
+            char name[11] = "gen0000.lif";
+            char number[10] = "0123456789";
+            for (int i = 0; i < 10; i++) {
+                if (gen / 1000 == i) {
+                name[3] = number[i];
+                }
+                if ((gen % 1000) / 100 == i) {
+                name[4] = number[i];
+                }
+            }
+            // データの書き込み
+            FILE *fq = fopen(name, "a+"); // 既存なら追加、未開ならファイルを作成
+            fprintf(fq, "#Life 1.06\n");
+            for (int i = 0; i < 40; i++) {
+                for (int j = 0; j < 70; j++) {
+                    if (cell[i][j] == 1) {
+                        fprintf(fq, "%d %d\n", j, i);
+                    }
+                }
+            } 
+            fclose(fq);
+        }
+        
+    }
+    
+    return EXIT_SUCCESS;
+}
